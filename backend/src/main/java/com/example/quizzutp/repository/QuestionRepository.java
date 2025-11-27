21a058a97192f18057e9728ebc08640da157756c
Pompinchuz <huillajesusantonio@gmail.com>
2025-11-27 17:45:44 -0500

    Correcion de QuestionRepo

--- backend/src/main/java/com/example/quizzutp/repository/QuestionRepository.java
+++ backend/src/main/java/com/example/quizzutp/repository/QuestionRepository.java
@@ -3,12 +3,13 @@ package com.example.quizzutp.repository;
 import com.example.quizzutp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    @Query(value = "SELECT * FROM questions ORDER BY RANDOM() LIMIT 5", nativeQuery = true)
-    List<Question> findRandomQuestions(int limit);
+    @Query(value = "SELECT * FROM questions ORDER BY RANDOM() LIMIT :limit", nativeQuery = true)
+    List<Question> findRandomQuestions(@Param("limit") int limit);
 }
